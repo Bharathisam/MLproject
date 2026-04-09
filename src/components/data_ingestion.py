0bda7e4d9a03d88900f0f170de08be44fe683b15
bhgh <132053890+Bharathisam@users.noreply.github.com>
2026-04-09 11:16:41 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -9,6 +9,8 @@ from src.exception import CustomException
 from src.logger import logging
 from src.components.data_transformation import DataTransformation   # ✅ IMPORTANT
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -57,8 +59,9 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
-    # ✅ Correct object creation
     data_transformation = DataTransformation()
+    train_arr, test_arr, preprocessor_path = data_transformation.initiate_data_transformation(train_data, test_data)
 
-    # ✅ Correct function call
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    modeltrainer = ModelTrainer()
+
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
